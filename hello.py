d1073cfa69efb466fa75974bc5b74713fea091a7
Tan Jing Kai <2602546@sit.singaporetech.edu.sg>
2026-09-04 11:15:23 +0800

    Followers gained

--- hello.py
+++ hello.py
@@ -2,11 +2,19 @@ username = input("Enter username: ")
 age = int(input("Enter age: "))
 category = input("Enter Content Category: ")
 
+followers = 6767
+
 print("\nInstagram Profile")
 print("=========================")
 print("Username: ", username)
 print("Age: ", age)
 print("Content Category: ", category)
+print("Day 1 followers: ", followers)
+followersgained = 50
+followers += followersgained
+print("Day 2 followers: ", followers)
+print("Followers gained: ", followersgained)
+
 
 
 if age>40 and category == "fun":
