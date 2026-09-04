11c34fa44f78536f1214d3475a584fb4af9320d4
Tan Jing Kai <2602546@sit.singaporetech.edu.sg>
2026-09-04 11:23:09 +0800

    -

--- hello.py
+++ hello.py
@@ -1,5 +1,5 @@
 username = input("Enter username: ")
-age = int(input("Enter age: "))
+age = int(input("Enter age: ")) 
 category = input("Enter Content Category: ")
 
 followers = 6767
@@ -7,9 +7,10 @@ followers = 6767
 print("\nInstagram Profile")
 print("=========================")
 print("Username: ", username)
-print("Age: ", age)
+print("Age: ", age, "years old")
 print("Content Category: ", category)
 print("Day 1 followers: ", followers)
+
 followersgained = 50
 followers += followersgained
 print("Day 2 followers: ", followers)
