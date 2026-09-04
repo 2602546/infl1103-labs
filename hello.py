7d551cc551f2b0b0033d723397560097585194ab
Tan Jing Kai <2602546@sit.singaporetech.edu.sg>
2026-09-04 10:50:03 +0800

    First Post

--- hello.py
+++ hello.py
@@ -2,3 +2,12 @@ print("=========================")
 print("Welcome here")
 print("My first post!")
 print("=========================")
+
+
+username = "Cream Reaper"
+bio = "Gooner"
+followers = "953967"
+
+print("Username: ", username)
+print("Bio: ", bio)
+print("Followers: ", followers)
