6a7d876321e1e9e96799c48b383f133e50d5f0f6
Tan Jing Kai <2602546@sit.singaporetech.edu.sg>
2026-09-11 11:44:00 +0800

    inventory

--- auditor.py
+++ auditor.py
@@ -1,1 +1,25 @@
-print("hello")
+inventory = 0
+f = 0
+
+while True:
+    s = input("Enter stock quantity or quit: ")
+
+    if s == "quit":
+        print("Total Units Processed:", inventory)
+        print("Number of Failed/Rejected Entries:", f)
+        break
+    elif not s.isdigit():
+        print("Error: Invalid input")
+        f += 1
+    else:
+        n = int(s)
+
+        if n < 0:
+            print("Error: Negative numbers are not allowed")
+            f += 1
+        else:
+            inventory += n
+
+            if inventory > 500:
+                print("Alert: Overstock")
+                break
