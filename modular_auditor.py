4f744c48e99db2152304254f1e687cdaaf65e6d3
2602546 <2602546@sit.singaporetech.edu.sg>
2026-09-19 14:38:26 +0800

    auditor copy

--- modular_auditor.py
+++ modular_auditor.py
@@ -1,48 +1,25 @@
-def get_valid_input():
-    s = input("Enter stock quantity or quit: ")
-
-    if s == "quit":
-        return "quit"
-    elif not s.isdigit():
-        print("Error: Invalid input")
-        return None
-    else:
-        n = int(s)
-        if n < 0:
-            print("Error: Negative numbers are not allowed")
-            return None
-        return n
-
-
-def process_delivery(current_total, new_value):
-    return current_total + new_value
-
-
-def calculate_tax(amount):
-    return amount * 0.10
-
-
-def generate_report(total_units, failed_attempts):
-    print("Total Units Processed:", total_units)
-    print("Number of Failed/Rejected Entries:", failed_attempts)
-
-
 inventory = 0
 f = 0
 
 while True:
-    result = get_valid_input()
+    s = input("Enter stock quantity or quit: ")
 
-    if result == "quit":
-        generate_report(inventory, f)
+    if s == "quit":
+        print("Total Units Processed:", inventory)
+        print("Number of Failed/Rejected Entries:", f)
         break
-    elif result is None:
+    elif not s.isdigit():
+        print("Error: Invalid input")
         f += 1
     else:
-        inventory = process_delivery(inventory, result)
-        tax = calculate_tax(result)
+        n = int(s)
 
-        if inventory > 500:
-            print("Alert: Overstock")
-            generate_report(inventory, f)
-            break
+        if n < 0:
+            print("Error: Negative numbers are not allowed")
+            f += 1
+        else:
+            inventory += n
+
+            if inventory > 500:
+                print("Alert: Overstock")
+                break
